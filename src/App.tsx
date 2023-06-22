236c7fcceb225eba1436e7f85470eb7cecd24add
mbaharip <62494292+mbahArip@users.noreply.github.com>
2023-06-22 09:26:32 +0700

    Fix: Test tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ function App() {
         <p className={"text-lg"}>Test</p>
         <p className="font-montserrat text-2xl">Test2</p>
       </section>
+      <section className="bg-gray-500">
+        <p>Lorem ipsum</p>
+      </section>
     </>
   );
 }
