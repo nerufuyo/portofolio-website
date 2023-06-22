6e8c054a3cf4a08fa08fd910b1d01aeed156324c
mbaharip <62494292+mbahArip@users.noreply.github.com>
2023-06-22 09:26:16 +0700

    Fix: Move config to extend instead

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,29 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+      spacing: {
+        sm: "8px",
+        md: "12px",
+        lg: "16px",
+        xl: "24px",
+      },
+      colors: {
+        bgColor: "#171717",
+        primaryColor: "#c2410c",
+        secondaryColor: "#d6d3d1",
+        tertiaryColor: "#f8fafc",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
     },
-    spacing: {
-      sm: "8px",
-      md: "12px",
-      lg: "16px",
-      xl: "24px",
-    },
-    colors: {
-      bgColor: "#171717",
-      primaryColor: "#c2410c",
-      secondaryColor: "#d6d3d1",
-      tertiaryColor: "#f8fafc",
-    },
-    fontFamily: {
-      poppins: ["Poppins", "sans-serif"],
-      montserrat: ["Montserrat", "sans-serif"],
-    },
-    extend: {},
   },
   plugins: [],
 };
